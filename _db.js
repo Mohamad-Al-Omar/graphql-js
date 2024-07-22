158f80b86c62a90bf7ae4a56052bd1a2a4e3592c
Mohamad Al Omar <m.al.omar94@gmail.com>
2024-07-23 02:56:01 +0400

    add database file

---
+++ _db.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    id: 1,
+    name: "User 1",
+  },
+  {
+    id: 2,
+    name: "User 2",
+  },
+  {
+    id: 3,
+    name: "User 3",
+  },
+];
+const posts = [
+  {
+    title: "Post 1",
+    content: "Content 1",
+    user_id: 1,
+  },
+  {
+    title: "Post 2",
+    content: "Content 2",
+    user_id: 2,
+  },
+];
+
+export default { users, posts };
